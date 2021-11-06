8ea3964d086f8e50e3736ae88cdc1cf263dbd787
115denis511 <115denis511@gmail.com>
2021-11-06 10:59:12 +0200

    fix

--- kbk-19-isp-4__SYS-LAB-8/main.cpp
+++ kbk-19-isp-4__SYS-LAB-8/main.cpp
@@ -20,41 +20,41 @@ int main()
 
 		// Поиск наибольшего значения в массиве
 		mov ecx, 10
-		loop_one:
+	loop_one:
 		cmp eax, dword ptr[ebx]
-			jg if_greater
-			mov eax, dword ptr[ebx]
-			if_greater :
-			add ebx, 4
-			loop loop_one
+		jg if_greater
+		mov eax, dword ptr[ebx]
+	if_greater :
+		add ebx, 4
+		loop loop_one
 
-			// Сохранение наибольшего
-			mov max, eax
+		// Сохранение наибольшего
+		mov max, eax
 
-			// Создание нового массива на основе предыдущего
-			mov ecx, 10
-			loop_two:
+		// Создание нового массива на основе предыдущего
+		mov ecx, 10
+	loop_two:
 		// Получение значения из i элемента основного массива
 		lea ebx, mass
-			mov eax, mass_offset
-			add ebx, eax
-			mov eax, dword ptr[ebx]
+		mov eax, mass_offset
+		add ebx, eax
+		mov eax, dword ptr[ebx]
 
-			// Разность полученного максимума и исходного элемента
-			mov edx, max
-			sub edx, eax
+		// Разность полученного максимума и исходного элемента
+		mov edx, max
+		sub edx, eax
 
-			// Запись в новый массив
-			lea ebx, result
-			mov eax, mass_offset
-			add ebx, eax
-			mov dword ptr[ebx], edx
+		// Запись в новый массив
+		lea ebx, result
+		mov eax, mass_offset
+		add ebx, eax
+		mov dword ptr[ebx], edx
 
-			// Сохранение смещения индекса для обоих массивов
-			mov eax, mass_offset
-			add eax, 4
-			mov mass_offset, eax
-			loop loop_two
+		// Сохранение смещения индекса для обоих массивов
+		mov eax, mass_offset
+		add eax, 4
+		mov mass_offset, eax
+		loop loop_two
 	}
 
 	std::cout << "max:\t" << max << std::endl;
